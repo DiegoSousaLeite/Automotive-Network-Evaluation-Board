dfdb300c7210be25cc16e0be83f796f5d7a8ecb9
Sooties <soousadiegoo@gmail.com>
2024-04-29 16:47:08 -0300

    Add class SystemProperties

---
+++ SystemProperties.h
@@ -0,0 +1,38 @@
+#ifndef SYSTEMPROPERTIES_H
+#define SYSTEMPROPERTIES_H
+
+class SystemProperties {
+public:
+    static constexpr auto TEST_RESULT_PROPERTY      = "test.result";
+    static constexpr auto TEST_STATE_PROPERTY       = "test.state";
+    static constexpr auto TEST_MESSAGE_PROPERTY     = "test.message";
+    static constexpr auto USER_DIRECTORY            = "user.dir";
+    static constexpr auto MCU_DIRECTORY             = "mcu.dir";
+    static constexpr auto MCU_APPLICATION           = "mcu.app";
+    static constexpr auto MCU_BOOTLOADER            = "mcu.boot";
+    static constexpr auto AVRDUDE_PROGRAM           = "avrdude.exe";
+    static constexpr auto AVRDUDE_CONFIG            = "avrdude.conf";
+    static constexpr auto MCU_OPT_PROG              = "mcu.opt.prog";
+    static constexpr auto ECU_OPT_PROG              = "ecu.opt.prog";
+    static constexpr auto MCU_OPT_FUSE              = "mcu.opt.fuse";
+    static constexpr auto MCU_OPT_BOOT1             = "mcu.opt.boot1";
+    static constexpr auto MCU_OPT_BOOT2             = "mcu.opt.boot2";
+    static constexpr auto MCU_PROG_BAUDRATE         = "mcu.prog.baudrate";
+    static constexpr auto ECU_PROG_BAUDRATE         = "ecu.prog.baudrate";
+    static constexpr auto ECU_APP_BAUDRATE          = "ecu.app.baudrate";
+    static constexpr auto MCU_APP_BAUDRATE          = "mcu.app.baudrate";
+    static constexpr auto MCU_PROG_USB              = "mcu.prog.usb";
+    static constexpr auto MCU_PROG_VID              = "mcu.prog.vid";
+    static constexpr auto MCU_PROG_PID              = "mcu.prog.pid";
+    static constexpr auto MCU_PROG_SERIAL           = "mcu.prog.serial";
+    static constexpr auto ECU_PORT_DESCRIPTION      = "ecu.port.desc";
+    static constexpr auto ECU_DESC_PORT_NAME        = "ecu.desc.port";
+    static constexpr auto MCU_PORT_DESCRIPTION      = "mcu.port.desc";
+    static constexpr auto MCU_DESC_PORT_NAME        = "mcu.desc.port";
+    static constexpr auto ECU_APPLICATION           = "ecu.app";
+    static constexpr auto ECU_DIRECTORY             = "ecu.dir";
+
+    static constexpr auto SYSTEM_APP_NAME           = "app.name";
+};
+
+#endif // SYSTEMPROPERTIES_H
