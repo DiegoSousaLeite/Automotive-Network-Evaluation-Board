39d129d4b9baacf3a7e758101ab0975aab4e21e4
Sooties <soousadiegoo@gmail.com>
2024-05-01 15:16:57 -0300

    Add LinNetworkTest

---
+++ LinNetworkTest.h
@@ -0,0 +1,53 @@
+#ifndef LINNETWORKTEST_H
+#define LINNETWORKTEST_H
+
+#include "GenericTest.h"
+#include "TestReportModel.h"
+#include "JigaTestInterface.h"
+#include <vector>
+
+class LinNetworkTest : public GenericTest {
+private:
+    static LinNetworkTest* instance;
+
+    LinNetworkTest() {
+        // Criando e configurando os modelos de teste para as placas específicas
+        TestReportModel* tModel;
+
+        // Modelo de teste para ECU3
+        tModel = new TestReportModel(JigaTestConstants::LIN_NETWORK_TEST, JigaTestConstants::ECU3_BOARD_ID);
+        tModel->addIndividualTest(JigaTestConstants::LIN1_ECU3_ITEST);
+        testReportModel.push_back(tModel);
+
+        // Modelo de teste para ECU4
+        tModel = new TestReportModel(JigaTestConstants::LIN_NETWORK_TEST, JigaTestConstants::ECU4_BOARD_ID);
+        tModel->addIndividualTest(JigaTestConstants::LIN1_ECU4_ITEST);
+        testReportModel.push_back(tModel);
+
+        // Modelo de teste para todos as placas
+        tModel = new TestReportModel(JigaTestConstants::LIN_NETWORK_TEST, JigaTestConstants::ALL_BOARDS_ID);
+        testReportModel.push_back(tModel);
+    }
+
+public:
+    ~LinNetworkTest() {
+        for (auto model : testReportModel) {
+            delete model;
+        }
+    }
+
+    static LinNetworkTest* getInstance() {
+        if (!instance) {
+            instance = new LinNetworkTest();
+        }
+        return instance;
+    }
+
+    LinNetworkTest(const LinNetworkTest&) = delete;
+    LinNetworkTest& operator=(const LinNetworkTest&) = delete;
+};
+
+// Inicialização do ponteiro estático de instância
+LinNetworkTest* LinNetworkTest::instance = nullptr;
+
+#endif // LINNETWORKTEST_H
