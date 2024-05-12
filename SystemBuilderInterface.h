2394cb0bc68b92fc596a23c4482cc006158dc0fb
Sooties <soousadiegoo@gmail.com>
2024-05-12 19:18:07 -0300

    add SystemBuilderInterface

---
+++ SystemBuilderInterface.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEMBUILDERINTERFACE_H
+#define SYSTEMBUILDERINTERFACE_H
+
+class SystemBuilderInterface
+{
+public:
+  virtual ~SystemBuilderInterface() {}
+
+    virtual void createSystem();
+};
+
+#endif // SYSTEMBUILDERINTERFACE_H
